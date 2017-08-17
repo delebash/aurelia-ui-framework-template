fd06fe949a824cac8e05efa5be9fc22d61015e90
dan <danelebash@hotmail.com>
2017-08-17 18:39:45 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var compass = require('gulp-compass'),
+  plumber = require('gulp-plumber');
+var del = require('del');
+var distFolder = './dist';
+
+// SASS/Compass compiler
+gulp.task('sass', function(done) {
+  return gulp.src('sass/**/*.scss')
+    .pipe(plumber({
+      errorHandler: function(error) {
+        console.log(error.message);
+        this.emit('end');
+      }
+    }))
+    .pipe(compass({
+      css: 'css',
+      config_file: 'sass/compass.rb'
+    }));
+});
+
+gulp.task('clean-dist', function() {
+  return del([distFolder + '/*', '!' + distFolder + '/CNAME', '!' + distFolder + '/Web.config', '!' + distFolder + '/crossdomain.xml'], {
+    force: true
+  });
+});
+
+gulp.task('copy-dist', function() {
+  return gulp.src(['./index.html',
+      './fonts/**/*', './css/**/*', './images/**/*', './glyphs/*', './locales/**/*', './favicons/**/*', './scripts/**/*'
+    ], {
+      base: './'
+    })
+    .pipe(gulp.dest(distFolder));
+});
+
+gulp.task('production', gulp.series('clean-dist', 'sass', 'copy-dist',
+  function(done) {
+    done();
+  }));
